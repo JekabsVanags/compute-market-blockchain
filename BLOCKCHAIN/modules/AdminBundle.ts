698b1f09f1593fcb2411c6e3bcc1c7def3c4dbb6
JekabsVanags <jecobv@gmail.com>
2025-12-02 16:22:17 +0200

    Removed cache and other stuff that shouldnt be there

--- BLOCKCHAIN/modules/AdminBundle.ts
+++
@@ -1,9 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-export default buildModule("AdminBundleModule", (m) => {
-  //Deploy the admin level contracts
-  const roles = m.contract("Roles");
-  const reputation = m.contract("Reputation", [roles]);
-
-  return { roles, reputation };
-});
